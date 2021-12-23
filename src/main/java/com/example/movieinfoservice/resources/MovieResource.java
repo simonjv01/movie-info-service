949adc99bbba7b3aee1e6f053b49abae2cfa2fc0
Simon Vargas <sjvargas01@gmail.com>
2021-12-23 15:32:29 -0500

    first commit

---
+++ src/main/java/com/example/movieinfoservice/resources/MovieResource.java
@@ -0,0 +1,22 @@
+package com.example.movieinfoservice.resources;
+
+
+import com.example.movieinfoservice.models.Movie;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.List;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieResource {
+
+    @RequestMapping("/{movieId}")
+    public Movie getMovieInfo(@PathVariable("movieId") String movieId) {
+        return new Movie(movieId, "Titanic");
+    }
+
+
+}
